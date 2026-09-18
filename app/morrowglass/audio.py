a5cc117c37dd97ed883153b7df20c11f3cb1d0c5
Quan Dang <danghohongquanst2k4@gmail.com>
2026-09-18 22:39:15 +0700

    morrowglass: expose official English Kokoro voice list

--- app/morrowglass/audio.py
+++ app/morrowglass/audio.py
@@ -16,6 +16,38 @@ KOKORO_EN_PREFIX = "kokoro-en:"
 KOKORO_VI_PREFIX = "kokoro-vi:"
 LEGACY_LOCAL_KOKORO_PREFIX = "kokoro-local:"
 
+ENGLISH_KOKORO_VOICES = (
+    "af_heart",
+    "af_alloy",
+    "af_aoede",
+    "af_bella",
+    "af_jessica",
+    "af_kore",
+    "af_nicole",
+    "af_nova",
+    "af_river",
+    "af_sarah",
+    "af_sky",
+    "am_adam",
+    "am_echo",
+    "am_eric",
+    "am_fenrir",
+    "am_liam",
+    "am_michael",
+    "am_onyx",
+    "am_puck",
+    "am_santa",
+    "bf_emma",
+    "bf_isabella",
+    "bf_alice",
+    "bf_lily",
+    "bm_george",
+    "bm_lewis",
+    "bm_daniel",
+    "bm_fable",
+)
+
+
 VIETNAMESE_KOKORO_VOICES = (
     "diem_trinh",
     "hung_thinh",
